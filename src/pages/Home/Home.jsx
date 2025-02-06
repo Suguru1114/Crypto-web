d8f2b7a792ea9bab8a6db5adf8ee8426dcf58205
Suguru Miyata <suguru1114@gmail.com>
2025-02-06 14:46:04 +1100

    change 24h text color

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -39,9 +39,15 @@ const Home = () => {
                 <img src={item.image} alt="" />
                 <p>{item.name + " - " +item.symbol}</p>
               </div>
-              <p>{currency.symbol} {item.current_price}</p>
-              <p>{Math.floor(item.price_change_percentage_24h*100)/100} %</p>
+              <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
+              <p className={item.price_change_percentage_24h > 0 ? "green" : "red"}>
+                {Math.floor(item.price_change_percentage_24h * 100) / 100} %
+                  </p>
+
               {/* addding percentage  */}
+
+              <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
+              {/* toLocaleString() adding . in numbers*/}
             </div>
           ))
         }
