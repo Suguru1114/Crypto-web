9035b2f9a3935fc8391c688959d861b7b44f94c7
Suguru Miyata <suguru1114@gmail.com>
2025-02-09 17:09:21 +1100

    add search functoin & empty arrey shows 10 items

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,20 +6,47 @@ const Home = () => {
 
   const {allCoin, currency} = useContext(CoinContext);
   const [displayCoin, setDisplayCoin] = useState([]);
+  const [input, setInput] = useState('');
+
+  const inputHandler = (event) =>{
+    setInput(event.target.value);
+    if(event.target.value === ""){
+      setDisplayCoin(allCoin);
+    }
+  }
+
+  const searchHandler = async (event)=>{
+    event.preventDefault();
+    const coins = await allCoin.filter((item)=>{
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins);
+  }
 
   useEffect(()=>{
     setDisplayCoin(allCoin);
   },[allCoin])
 
   return (
-    <div className='home'>
+    <div className='home'> 
       <div className='hero'>
         <h1>Largest <br/> Crypto Marketplace</h1>
         <p>Welcome to the world's largest cryptocurrency
         marketplace. Sign up to explore more about cryptos.</p>
-        <form>
-          <input type="text" placeholder='Search crypto..'/>
+        <form onSubmit={searchHandler}>
+
+          <input onChange={inputHandler} value={input} type="text" placeholder='Search crypto..' required/>
+        
+          <datalist id="coinlist">
+           {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+          
+          
+          </datalist>
+
+
+
           <button type="submit">Search</button>
+
         </form>
       </div>
       <div className="crypto-table">
