a3b0b6bcf05d36faf2748daad7310521c33ddf9c
Suguru Miyata <suguru1114@gmail.com>
2025-02-10 00:04:21 +1100

    trying to fetch coin data

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import './Coin.css'
 import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext';
 
 
 function Coin() {
 
   const {coinId} = useParams();
+  const [coinData, setCoinData] = useState();
+  const {currency} = useContext(CoinContext);
+
+  const fetchCoinData = async ()=>{
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch(`https://pro-api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(res => res.json())
+      .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+    }
+
+    useEffect(()=>{
+      fetchCoinData();
+     },[currency])
 
   return (
-    <div>
-      <h2>Coin : {coinId}</h2>
+    <div className='coin'>
+      <div className='coin-name'>
+        <img src={coinData.image.large} alt=" "/>
+        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+      </div>
     </div>
   )
 }
